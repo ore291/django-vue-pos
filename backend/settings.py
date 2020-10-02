89daf69a90f7e2a19005c3d8caeb27d80f7af4a3
ore291 <31942749+ore291@users.noreply.github.com>
2020-10-02 13:43:00 +0100

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'VERY_SECRET_KEY_THAT_YOU_SHOUL
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://django-vue-pos.herokuapp.com/','localhost']
+ALLOWED_HOSTS = ['https://django-vue-pos.herokuapp.com','localhost']
 
 # Application definition
 
