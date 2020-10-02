e6f8f3e1cacf7b957f1c83c4921c7812e29c1d37
ore291 <31942749+ore291@users.noreply.github.com>
2020-10-02 14:20:33 +0100

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -144,7 +144,7 @@ REST_FRAMEWORK = {
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 WHITENOISE_INDEX_FILE = True
 
