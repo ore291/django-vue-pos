5e2d6c0e648c9f445670cd5626b7c9b0b023b6eb
ore291 <31942749+ore291@users.noreply.github.com>
2020-10-03 19:43:34 +0100

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -148,7 +148,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
-CORS_ORIGIN_WHITELIST = ['django-vue-pos.herokuapp.com']
 
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
