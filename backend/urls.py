0baa0942ae5973771d13b1f6ae9ac58aef0e82ff
ore291 <31942749+ore291@users.noreply.github.com>
2020-10-03 10:31:14 +0100

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     url('', include(authe.urls)),
     url(r'^api/', include(api.urls), name='api'),
     url(r'^(?!admin|api|static).*$', IndexView.as_view())
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
 
 if settings.DEBUG:
     urlpatterns + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT )
