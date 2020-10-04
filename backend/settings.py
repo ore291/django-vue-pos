0dcbc0a9591c6500631580e7e897fab9cbd1faa4
ore291 <oreoluwapadonu@gmail.com>
2020-10-04 22:59:19 +0100

    .

--- backend/settings.py
+++ backend/settings.py
@@ -82,7 +82,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
